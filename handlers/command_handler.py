e77d9c08c8bd127fee659eb03bbe037cf92c1575
VladyslavRua <helldemonvlad@gmail.com>
2026-03-15 12:57:16 +0000

    command_handler completed

--- handlers/command_handler.py
+++ handlers/command_handler.py
@@ -27,6 +27,24 @@ class CommandHandler:
             "змінити-адресу": self.edit_address,
             "змінити-день-народження": self.edit_birthday,
             "видалити-контакт": self.delete_contact,
+            "видалити-телефон": self.delete_phone,
+            "знайти-контакт": self.find_contact,
+            "показати-контакт": self.show_contact,
+            "показати-контакти": self.show_contacts,
+            "дні-народження": self.birthdays,
+            "додати-нотатку": self.add_note,
+            "змінити-нотатку": self.edit_note,
+            "видалити-нотатку": self.delete_note,
+            "додати-тег": self.add_tag,
+            "видалити-тег": self.remove_tag,
+            "знайти-нотатку": self.find_note,
+            "знайти-тег": self.find_tag,
+            "показати-нотатки": self.show_notes,
+            "сортувати-нотатки": self.sort_notes,
+            "зберегти": self.save,
+            "експорт-csv": self.export_csv,
+            "вихід": self.exit_app,
+            "закрити": self.exit_app,
         }
 
     # обробка команд
@@ -49,7 +67,38 @@ class CommandHandler:
 
     # вивести хелпер
     def help_command(self, args: list[str]) -> str:
-        pass
+        return (
+            "Доступні команди:\n"
+            "привіт\n"
+            "допомога\n"
+            "додати-контакт <ім'я>\n"
+            "додати-телефон <ім'я> <телефон>\n"
+            "додати-email <ім'я> <email>\n"
+            "додати-адресу <ім'я> <адреса>\n"
+            "додати-день-народження <ім'я> <DD.MM.YYYY>\n"
+            "змінити-телефон <ім'я> <старий_телефон> <новий_телефон>\n"
+            "змінити-email <ім'я> <новий_email>\n"
+            "змінити-адресу <ім'я> <нова_адреса>\n"
+            "змінити-день-народження <ім'я> <DD.MM.YYYY>\n"
+            "видалити-контакт <ім'я>\n"
+            "видалити-телефон <ім'я> <телефон>\n"
+            "знайти-контакт <запит>\n"
+            "показати-контакт <ім'я>\n"
+            "показати-контакти\n"
+            "дні-народження <кількість_днів>\n"
+            "додати-нотатку <текст>\n"
+            "змінити-нотатку <id> <новий_текст>\n"
+            "видалити-нотатку <id>\n"
+            "додати-тег <id> <тег>\n"
+            "видалити-тег <id> <тег>\n"
+            "знайти-нотатку <запит>\n"
+            "знайти-тег <тег>\n"
+            "показати-нотатки\n"
+            "сортувати-нотатки\n"
+            "зберегти\n"
+            "експорт-csv\n"
+            "закрити / вихід"
+        )
 
     # Додає контакт
     @input_error
@@ -101,5 +150,94 @@ class CommandHandler:
     def delete_contact(self, args: list[str]) -> str:
         return self.contact_service.delete_contact(args[0])
 
+    # Видаляє телефон
+    @input_error
+    def delete_phone(self, args: list[str]) -> str:
+        return self.contact_service.delete_phone(args[0], args[1])
 
+    # Шукає контакт
+    @input_error
+    def find_contact(self, args: list[str]) -> str:
+        return self.contact_service.find_contact(" ".join(args))
 
+    # Показує один контакт
+    @input_error
+    def show_contact(self, args: list[str]) -> str:
+        return self.contact_service.show_contact(args[0])
+
+    # Показує всі контакти
+    def show_contacts(self, args: list[str]) -> str:
+        return self.contact_service.show_all_contacts()
+
+    # Показує найближчі дні народження
+    @input_error
+    def birthdays(self, args: list[str]) -> str:
+        return self.contact_service.upcoming_birthdays(int(args[0]))
+
+    # Додає нотатку
+    @input_error
+    def add_note(self, args: list[str]) -> str:
+        return self.note_service.add_note(" ".join(args))
+
+    # Редагує нотатку
+    @input_error
+    def edit_note(self, args: list[str]) -> str:
+        return self.note_service.edit_note(int(args[0]), " ".join(args[1:]))
+
+    # Видаляє нотатку
+    @input_error
+    def delete_note(self, args: list[str]) -> str:
+        return self.note_service.delete_note(int(args[0]))
+
+    # Додає тег
+    @input_error
+    def add_tag(self, args: list[str]) -> str:
+        return self.note_service.add_tag(int(args[0]), args[1])
+
+    # Видаляє тег
+    @input_error
+    def remove_tag(self, args: list[str]) -> str:
+        return self.note_service.remove_tag(int(args[0]), args[1])
+
+    # Шукає нотатку
+    @input_error
+    def find_note(self, args: list[str]) -> str:
+        return self.note_service.find_notes(" ".join(args))
+
+    # Шукає нотатки за тегом
+    @input_error
+    def find_tag(self, args: list[str]) -> str:
+        return self.note_service.find_notes_by_tag(args[0])
+
+    # Показує всі нотатки
+    def show_notes(self, args: list[str]) -> str:
+        return self.note_service.show_all_notes()
+
+    # Сортує нотатки за тегами
+    def sort_notes(self, args: list[str]) -> str:
+        return self.note_service.sort_notes_by_tags()
+
+    # Зберігає всі дані
+    def save(self, args: list[str]) -> str:
+        self.repository.save_all(
+            self.contact_service.contact_book,
+            self.note_service.notes_book,
+        )
+        return "Дані успішно збережено."
+
+    # Експортує дані у CSV
+    def export_csv(self, args: list[str]) -> str:
+        self.repository.save_all(
+            self.contact_service.contact_book,
+            self.note_service.notes_book,
+        )
+        self.repository.export_all_to_csv()
+        return "Дані успішно експортовано у CSV."
+
+    # Завершує роботу програми
+    def exit_app(self, args: list[str]) -> str:
+        self.repository.save_all(
+            self.contact_service.contact_book,
+            self.note_service.notes_book,
+        )
+        return "До побачення!"
